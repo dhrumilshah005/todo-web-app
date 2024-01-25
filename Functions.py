6676d5d4dad8fa0d01f6019942384a5e4cb056a3
Dhrumil Shah <dhrumilshah2007@gmail.com>
2024-01-25 18:15:12 +0530

    Initial Commit

---
+++ Functions.py
@@ -0,0 +1,14 @@
+filepath = "todos.txt"
+def get_todos(filepath = "todos.txt"):
+    with open(filepath, "r") as old_file:
+        todos_old = old_file.readlines()
+    return todos_old
+
+def new_todos(todos,filepath="todos.txt"):
+    with open(filepath, 'w') as new_file:
+        new_todos = new_file.writelines(todos)
+    return new_todos
+
+
+if __name__ == "__main__":
+    print("Heallo")
---
+++ webapp.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import Functions
+import time
+
+existing_todos = Functions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    existing_todos.append(todo)
+    Functions.new_todos(existing_todos)
+    time.sleep(0.1)
+    st.session_state["new_todo"] =""
+
+def complete_todo(todo):
+    existing_todos.remove(todo)
+    Functions.new_todos(existing_todos)
+    st.rerun()
+
+
+text = st.empty()
+st.title("My todo app")
+st.text_input(label="",placeholder="Enter a task",on_change=add_todo,key='new_todo')
+
+for i,j in enumerate(existing_todos):
+    checkbox = st.checkbox(j,key=j)
+    if checkbox:
+        existing_todos.remove(j)
+        Functions.new_todos(existing_todos)
+        st.rerun()
